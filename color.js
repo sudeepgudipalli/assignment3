198ecb387439ee7a8a075da4cfbaa84f7892e2cd
sudeepgudipalli <119794609+sudeepgudipalli@users.noreply.github.com>
2022-12-04 18:26:11 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,13 @@
+const Body = document.body;
+const txt = document.getElementById("h1");
+const btn = document.getElementById("bgcolor");
+
+const bgcolor = ['tomato','midnightblue','purple','skyblue','slateblue','mediumseagreen'];
+
+btn.addEventListener('click',change);
+
+function change(){
+    const ind = Math.floor(Math.random()*7);
+    Body.style.backgroundColor = bgcolor[ind];
+    txt.style.color = 'white';
+}
